33dd27ffbb50e996862b67620ff9dec2a82167a2
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-25 11:26:20 -0300

    Create the service mock

---
+++ test/mock/service/client-service-mock.ts
@@ -0,0 +1,32 @@
+import { ClientEntity } from '@/domain/entities'
+import { ClientServiceContract } from '@/services/contract/client-contract'
+import { ClientCreateServiceError, ClientLoadServiceError } from '@/services/error'
+import { Either, right, left } from '@/shared/errors/Either'
+import { ClientModel } from '@/use-case/model'
+import { ClientViews } from '@/views/client-views'
+import { faker } from '@faker-js/faker'
+
+export class ClientServiceMock implements ClientServiceContract {
+  private clientMock: ClientEntity[] = []
+  async create(data: ClientEntity): Promise<Either<ClientCreateServiceError, ClientModel>> {
+    try {
+      data.idCliente = 1
+      this.clientMock.push(data)
+      const clientView = new ClientViews()
+      const resultView = clientView.fromEntity(data)
+      return right(resultView)
+    } catch (error: any) {
+      return left(new ClientCreateServiceError(error.message))
+    }
+  }
+
+  async load(): Promise<Either<ClientLoadServiceError, ClientModel[]>> {
+    try {
+      const clientView = new ClientViews()
+      const resultView = this.clientMock.map<ClientModel>(item => clientView.fromEntity(item))
+      return right(resultView)
+    } catch (error: any) {
+      return left(new ClientCreateServiceError(error.message))
+    }
+  }
+}
