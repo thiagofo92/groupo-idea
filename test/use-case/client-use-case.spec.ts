59a802905105e990c6859275e2c6f0219627a20a
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-24 21:42:13 -0300

    Create class ClienUseCase

--- test/use-case/client-use-case.spec.ts
+++ test/use-case/client-use-case.spec.ts
@@ -1,20 +1,51 @@
 import { describe, vi, expect, test } from 'vitest'
 
 
+interface ClientModel {
+  idClient: number
+  nome: string
+}
+
+interface Client {
+  nome: string
+cpf: string
+dtNascimento: Date
+ativo: boolean
+}
+
+class ClientEntity {
+  idCliente?: number
+  nome: string
+  cpf: string
+  dtNascimento: Date
+  ativo: boolean
+
+  constructor({nome, cpf, dtNascimento, ativo}: Client) {
+
+    if(dtNascimento.getFullYear() - new Date().getFullYear() < 18) {
+    }
+
+    this.nome = nome
+    this.cpf = cpf
+    this.dtNascimento = dtNascimento
+    this.ativo = ativo
+  }
+}
+
 
 interface BaseUseCaseContract<T, K> {
   create: (data: T) => Promise<any>
   load: () => Promise<K[]>
 }
 
-interface ClientUseCaseContract extends BaseUseCaseContract <any, any> {
-  create: (data: any) => Promise<boolean>
+interface ClientUseCaseContract extends BaseUseCaseContract <ClientEntity, ClientModel> {
+  create: (data: ClientEntity) => Promise<ClientModel>
   load: () => Promise<[]>
 }
 
 
 export class ClientUseCase implements ClientUseCaseContract {
-  async create (data: any): Promise<boolean> {
+  async create (data: ClientEntity): Promise<ClientModel> {
     return true
   }
 
