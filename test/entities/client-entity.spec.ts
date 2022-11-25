4a8e7ad17cb87899d2991aa58d81cc297781d390
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-25 17:05:43 -0300

    Update

--- test/entities/client-entity.spec.ts
+++ test/entities/client-entity.spec.ts
@@ -5,10 +5,10 @@ import { ClientEntity } from '@/domain/entities/client-entity'
 describe('# Use case create a client', () => {
   test('Client has the major age', async () => {
     const clientMock = {
-      nome: faker.name.fullName(),
-      dtNascimento: faker.date.birthdate({ min: 18, max: 125, mode: 'age'}),
+      name: faker.name.fullName(),
+      birthday: faker.date.birthdate({ min: 18, max: 125, mode: 'age'}),
       cpf: '11122233344',
-      ativo: true
+      active: true
     }
 
     const client = new ClientEntity(clientMock)
@@ -18,10 +18,10 @@ describe('# Use case create a client', () => {
 
   test('Error to create the client that is under eighteen years old', async () => {
     const clientMock = {
-      nome: faker.name.fullName(),
-      dtNascimento: faker.date.birthdate({ min: 10, max: 16, mode: 'age'}),
+      name: faker.name.fullName(),
+      birthday: faker.date.birthdate({ min: 10, max: 16, mode: 'age'}),
       cpf: '11122233344',
-      ativo: true
+      active: true
     }
 
     const client = new ClientEntity(clientMock)
