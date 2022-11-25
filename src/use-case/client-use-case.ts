1bb9812f8b03f56c75be5d3defb75315fc12dd6a
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-25 11:24:01 -0300

    Return the errors, ClientCreateUseCaseError and ClientCreateServiceError

--- src/use-case/client-use-case.ts
+++ src/use-case/client-use-case.ts
@@ -1,19 +1,30 @@
 import { ClientEntity } from '@/domain/entities'
-import { Either, right } from '@/shared/errors/Either'
+import { ClientServiceContract } from '@/services/contract/client-contract'
+import { ClientCreateServiceError, ClientLoadServiceError } from '@/services/error'
+import { Either, left, right } from '@/shared/errors/Either'
 import { ClientUseCaseContract } from './contract'
 import { ClientCreateUseCaseError } from './error'
 import { ClientModel } from './model'
 
 export class ClientUseCase implements ClientUseCaseContract {
-  constructor(private readonly clientService: ClientService) {}
+  constructor(private readonly clientService: ClientServiceContract) {}
 
-  async create (data: ClientEntity): Promise<Either<ClientCreateUseCaseError, ClientModel>> {
+  async create (data: ClientEntity): Promise<Either<ClientCreateUseCaseError | ClientCreateServiceError, ClientModel>> {
+    if(!data.legalAge()) return left(new ClientCreateUseCaseError('Under the age'))
+    
     const result = await this.clientService.create(data)
-    return right(result)
+
+    if(result.isLeft()) return left(result.value)
+
+    return right(result.value as any)
   }
 
-  async load(): Promise<Either<Error, any[]>> {
-    return {} as any
+  async load(): Promise<Either<ClientLoadServiceError, ClientModel[]>> {
+    const result = await this.clientService.load()
+
+    if(result.isLeft()) return left(result.value)
+
+    return right(result.value as any)
   }
 
 }
