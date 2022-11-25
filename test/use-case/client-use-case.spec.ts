2646028a948bc19b154a9ae164e2af982a078dea
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-25 10:02:31 -0300

    Rename path to folder usecase

--- test/use-case/client-use-case.spec.ts
+++ test/use-case/client-use-case.spec.ts
@@ -2,24 +2,8 @@ import { describe, vi, expect, test } from 'vitest'
 import { faker } from '@faker-js/faker'
 import { Either, right } from '@/shared/errors/Either'
 import { ClientEntity } from '@/domain/entities/client-entity'
-import { ClientUseCaseContract } from '@/usecase/contract'
-import { ClientCreateUseCaseError } from '@/usecase/error'
-import { ClientModel } from '@/usecase/model'
+import { ClientUseCase } from '@/use-case'
 
-export class ClientUseCase implements ClientUseCaseContract {
-  async create (data: ClientEntity): Promise<Either<ClientCreateUseCaseError, ClientModel>> {
-    const client: ClientModel = {
-      idClient: 1,
-      nome: data.nome
-    }
-    return right(client)
-  }
-
-  async load(): Promise<Either<Error, any[]>> {
-    return {} as any
-  }
-
-}
 
 function factoryClienUseCase() {
   const sut = new ClientUseCase()
