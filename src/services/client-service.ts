f707dedd62912f69194d19d029c34018f9e918df
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-25 11:22:20 -0300

    Create the function create and load, but the class are not using the connection to DB

---
+++ src/services/client-service.ts
@@ -0,0 +1,40 @@
+import { ClientEntity } from '@/domain/entities'
+import { Either, left, right } from '@/shared/errors/Either'
+import { ClientServiceContract } from './contract/client-contract'
+import { ClientCreateServiceError, ClientLoadServiceError } from './error'
+
+export class ClientService implements ClientServiceContract  {
+  async create(): Promise<Either<ClientCreateServiceError, ClientEntity>> {
+    try {
+      const result: ClientEntity = {
+        nome: '',
+        cpf: '',
+        dtNascimento: new Date(new Date().setFullYear(1992)),
+        ativo: true,
+        legalAge: function (): boolean {
+          throw new Error('Function not implemented.')
+        }
+      }
+      return right(result)
+    } catch (error: any) {
+      return left(new ClientCreateServiceError(error.message))
+    }
+  }
+
+  async load(): Promise<Either<ClientLoadServiceError, ClientEntity[]>> {
+    try {
+      const result: ClientEntity = {
+        nome: '',
+        cpf: '',
+        dtNascimento: new Date(new Date().setFullYear(1992)),
+        ativo: true,
+        legalAge: function (): boolean {
+          throw new Error('Function not implemented.')
+        }
+      }
+      return right([result])
+    } catch (error: any) {
+      return left(new ClientCreateServiceError(error.message))
+    }
+  }
+}
