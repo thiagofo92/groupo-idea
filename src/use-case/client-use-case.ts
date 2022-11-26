a9982e92fdb122a13fe4cac77c5db7dfdfd88fa6
Thigo Oliveira <thiagofo92@gmail.com>
2022-11-26 19:46:52 -0300

    Refactory to input data be ClientCreateModel and responde be ClientCreateResponse

--- src/use-case/client-use-case.ts
+++ src/use-case/client-use-case.ts
@@ -4,27 +4,34 @@ import { ClientCreateServiceError, ClientLoadServiceError } from '@/services/err
 import { Either, left, right } from '@/shared/errors/Either'
 import { ClientUseCaseContract } from './contract'
 import { ClientCreateUseCaseError } from './error'
-import { ClientModel } from './model'
+import { ClientCreateModel, ClientCreateResponseModel } from './model'
 
 export class ClientUseCase implements ClientUseCaseContract {
   constructor(private readonly clientService: ClientServiceContract) {}
 
-  async create (data: ClientEntity): Promise<Either<ClientCreateUseCaseError | ClientCreateServiceError, ClientModel>> {
-    if(!data.legalAge()) return left(new ClientCreateUseCaseError('Under the age'))
+  async create (data: ClientCreateModel): 
+  Promise<Either<ClientCreateUseCaseError | ClientCreateServiceError, ClientCreateResponseModel>> {
+
+    const client = new ClientEntity({
+      name: data.nome,
+      cpf: data.cpf,
+      birthday: data.dtNascimento
+    })
+
+    if(!client.legalAge()) return left(new ClientCreateUseCaseError('Under the age'))
     
-    const result = await this.clientService.create(data)
+    const result = await this.clientService.create(client)
 
     if(result.isLeft()) return left(result.value)
 
     return right(result.value as any)
   }
 
-  async load(): Promise<Either<ClientLoadServiceError, ClientModel[]>> {
+  async load(): Promise<Either<ClientLoadServiceError, ClientCreateResponseModel[]>> {
     const result = await this.clientService.load()
 
     if(result.isLeft()) return left(result.value)
 
     return right(result.value as any)
   }
-
 }
--- src/use-case/model/client-model.ts
+++ src/use-case/model/client-model.ts
@@ -1,4 +1,10 @@
-export interface ClientModel {
+export interface ClientCreateModel {
+  cpf: string
+  dtNascimento: Date
+  nome: string
+}
+
+export interface ClientCreateResponseModel {
   idClient: number
   nome: string
 }
