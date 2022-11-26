343bf37eb9a8a5bded1666664ca838584c703df7
Thigo Oliveira <thiagofo92@gmail.com>
2022-11-26 19:44:17 -0300

    Refactory to input data be ClientCreateModel and responde be ClientCreateResponse

--- src/services/contract/client-contract.ts
+++ src/services/contract/client-contract.ts
@@ -1,9 +1,9 @@
 import { ClientEntity } from '@/domain/entities'
 import { Either } from '@/shared/errors/Either'
-import { ClientModel } from '@/use-case/model'
+import { ClientCreateModel } from '@/use-case/model'
 import { ClientCreateServiceError, ClientLoadServiceError } from '../error'
 
 export interface ClientServiceContract {
-  create: (data: ClientEntity) => Promise<Either<ClientCreateServiceError, ClientModel>> 
-  load: () => Promise<Either<ClientLoadServiceError, ClientModel[]>>
+  create: (data: ClientEntity) => Promise<Either<ClientCreateServiceError, ClientCreateModel>> 
+  load: () => Promise<Either<ClientLoadServiceError, ClientCreateModel[]>>
 }
