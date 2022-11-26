00c7f2b1a48b198865b676fab8514cfe019c47ba
Thigo Oliveira <thiagofo92@gmail.com>
2022-11-26 16:46:08 -0300

    Created the product service mock to make tests

--- test/mock/service/product-service-mock.ts
+++ test/mock/service/product-service-mock.ts
@@ -1,19 +1,23 @@
+import { ProductEntity } from '@/domain/entities'
 import { ProductServiceContract } from '@/services/contract'
 import { ProductCreateServiceError, ProductLoadServiceError } from '@/services/error'
 import { Either, left, right } from '@/shared/errors/Either'
 
 export class ProductServiceMock implements ProductServiceContract {
-  async create (): Promise<Either<ProductCreateServiceError, any>> {
+  private readonly productEntity: ProductEntity [] = []
+  async create (data: ProductEntity): Promise<Either<ProductCreateServiceError, ProductEntity>> {
     try {
-      return right({} as any) 
+      data.idProduct = 1
+      this.productEntity.push(data)
+      return right(data) 
     } catch (error: any) {
       return left(new ProductCreateServiceError(error.message))
     }
   }
 
-  async load (): Promise<Either<ProductLoadServiceError, any>> {
+  async load (): Promise<Either<ProductLoadServiceError, ProductEntity[]>> {
     try {
-      return right({} as any)
+      return right(this.productEntity)    
     } catch (error: any) {
       return left(new ProductLoadServiceError(error.message))
     }
