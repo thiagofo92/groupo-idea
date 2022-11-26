2edccc4c27ea41ae2c20d5fa50b788f074f747c8
Thigo Oliveira <thiagofo92@gmail.com>
2022-11-26 16:44:06 -0300

    Add propertie to function create

--- src/use-case/product-use-case.ts
+++ src/use-case/product-use-case.ts
@@ -3,15 +3,22 @@ import { ProductServiceContract } from '@/services/contract'
 import { ProductCreateServiceError, ProductLoadServiceError } from '@/services/error'
 import { Either, left, right } from '@/shared/errors/Either'
 import { ProductUseCaseContract } from './contract/product-contract'
+import { ProductModel } from './model/product-model'
 
 export class ProductUseCase implements ProductUseCaseContract {
   constructor(private readonly productService: ProductServiceContract) {}
-  async create (): Promise<Either<ProductCreateServiceError, ProductEntity>> {
-    const result = await this.productService.create()
-    return right({} as any) 
+  async create (data: ProductModel): Promise<Either<ProductCreateServiceError, ProductEntity>> {
+    const createdProduct = await this.productService.create(data)
+    if(createdProduct.isRight()) return right(createdProduct.value)
+
+    return left(createdProduct.value)
   }
 
   async load (): Promise<Either<ProductLoadServiceError, ProductEntity[]>> {
-    return right({} as any)
+    const productData = await this.productService.load()
+
+    if(productData.isRight()) return right(productData.value)
+
+    return left(productData.value)
   }
 }
