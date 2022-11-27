7e303b829c218a4acffb423a8c1bb7574ae13345
Thigo Oliveira <thiagofo92@gmail.com>
2022-11-27 19:23:34 -0300

    Refactory to use model

--- src/services/contract/product-contract.ts
+++ src/services/contract/product-contract.ts
@@ -1,9 +1,9 @@
 import { ProductEntity } from '@/domain/entities'
 import { Either } from '@/shared/errors/Either'
-import { ProductModel } from '@/use-case/model'
+import { ProducCreatetModel } from '@/use-case/model'
 import { ProductCreateServiceError, ProductLoadServiceError } from '../error'
 
 export interface ProductServiceContract {
-  create: (data: ProductEntity) => Promise<Either<ProductCreateServiceError, ProductModel>>
-  load: () => Promise<Either<ProductLoadServiceError, ProductModel[]>>
+  create: (data: ProductEntity) => Promise<Either<ProductCreateServiceError, ProducCreatetModel>>
+  load: () => Promise<Either<ProductLoadServiceError, ProducCreatetModel[]>>
 }
