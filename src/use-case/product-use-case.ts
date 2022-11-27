d54f21be64fcce34dff1e3143581bac7f3bdd784
Thigo Oliveira <thiagofo92@gmail.com>
2022-11-27 20:57:47 -0300

    Refactory to use case return a model type

--- src/use-case/product-use-case.ts
+++ src/use-case/product-use-case.ts
@@ -3,18 +3,18 @@ import { ProductServiceContract } from '@/services/contract'
 import { ProductCreateServiceError, ProductLoadServiceError } from '@/services/error'
 import { Either, left, right } from '@/shared/errors/Either'
 import { ProductUseCaseContract } from './contract/product-contract'
-import { ProducCreatetModel } from './model/product-model'
+import { ProducCreatetModel, ProductCreateResponseModel } from './model/product-model'
 
 export class ProductUseCase implements ProductUseCaseContract {
   constructor(private readonly productService: ProductServiceContract) {}
-  async create (data: ProducCreatetModel): Promise<Either<ProductCreateServiceError, ProductEntity>> {
+  async create (data: ProducCreatetModel): Promise<Either<ProductCreateServiceError, ProductCreateResponseModel>> {
     const createdProduct = await this.productService.create(data)
     if(createdProduct.isRight()) return right(createdProduct.value)
 
     return left(createdProduct.value)
   }
 
-  async load (): Promise<Either<ProductLoadServiceError, ProductEntity[]>> {
+  async load (): Promise<Either<ProductLoadServiceError, ProductCreateResponseModel[]>> {
     const productData = await this.productService.load()
 
     if(productData.isRight()) return right(productData.value)
