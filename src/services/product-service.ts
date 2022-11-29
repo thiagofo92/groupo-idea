62f60e57170744b2de53ef774b2ce5d0c96381d5
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-28 21:35:12 -0300

    Update

--- src/services/product-service.ts
+++ src/services/product-service.ts
@@ -2,14 +2,25 @@ import { ProductEntity } from '@/domain/entities'
 import { Either, left, right } from '@/shared/errors/Either'
 import { ProductServiceContract } from './contract'
 import { ProductCreateServiceError, ProductLoadServiceError } from './error'
-
+import { Prisma } from './db/prisma'
+import { ProductCreateResponseModel } from '@/use-case/model'
 export class ProductService implements ProductServiceContract {
   private readonly productEntity: ProductEntity [] = []
   async create (data: ProductEntity): Promise<Either<ProductCreateServiceError, ProductEntity>> {
     try {
-      data.idProduct = 1
-      this.productEntity.push(data)
-      return right(data) 
+      const createdProduct = await Prisma.product.create({
+        data: {
+          name: data.name,
+          active: data.active
+        }
+      })
+      const formated: ProductCreateResponseModel = {
+        idProduct: createdProduct.id,
+        name: createdProduct.name,
+        active: createdProduct.active
+      }
+
+      return right(formated) 
     } catch (error: any) {
       return left(new ProductCreateServiceError(error.message))
     }
@@ -17,7 +28,15 @@ export class ProductService implements ProductServiceContract {
 
   async load (): Promise<Either<ProductLoadServiceError, ProductEntity[]>> {
     try {
-      return right(this.productEntity)    
+      const loadProduct = await Prisma.product.findMany()
+
+      const formated = loadProduct.map<ProductCreateResponseModel>(item => ({
+        idProduct: item.id,
+        name: item.name,
+        active: item.active
+      }))
+
+      return right(formated)
     } catch (error: any) {
       return left(new ProductLoadServiceError(error.message))
     }
