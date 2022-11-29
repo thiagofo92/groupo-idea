1c79497496f5b9d005e60c081a002fcb14b02ee1
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-29 00:35:20 -0300

    Create validation parameters to client controller

--- src/main/routers/middlewares/client-middleware.ts
+++ src/main/routers/middlewares/client-middleware.ts
@@ -1,4 +1,23 @@
 import * as Yup from 'yup'
+import { Request, Response, NextFunction } from 'express'
+import { badRequest } from '@/controllers/helpers/http-respnse'
 
+const schema = Yup.object().shape({
+  nome: Yup.string().required(),
+  cpf: Yup.string().required().matches(/^([0-9]{3}).([0-9]{3}).([0-9]{3})-([0-9]{2})$/g),
+  dtNascimento: Yup.date()
+})
 
-const schema = Yup.
+export async function clientCreateMiddleware(request: Request, response: Response, next: NextFunction) {
+  try {
+    const isValid = await schema.validate(request.body, { abortEarly: false })
+    console.log(isValid)
+    
+    next()
+  } catch (error: any) {
+    const params = error.inner.map((item: any) => item.path).join(', ')
+    const message = `Invalid parameter ${params}`
+    const result = badRequest(message)
+    response.status(result.statusCode).json(result.data)
+  }
+}
