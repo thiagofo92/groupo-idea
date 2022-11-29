1493ae08112c453e6315aa1c7414f4861f60d17f
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-29 11:34:47 -0300

    Create the response for cliend id and product id not found

--- src/controllers/order-controller.ts
+++ src/controllers/order-controller.ts
@@ -1,7 +1,8 @@
 import { RequestContract } from '@/main/contracts/http-contract'
 import { OrderUseCaseContract } from '@/use-case/contract'
+import { OrderUseCaseClientNotFoundError, OrderUseCaseProductNotFoundError } from '@/use-case/error/order-error'
 import { HttpResponse } from './contracts/http-responde'
-import { internalError, success, successToCreate } from './helpers/http-respnse'
+import { badRequest, internalError, success, successToCreate } from './helpers/http-respnse'
 import { OrderModel } from './models'
 
 export class OrderController {
@@ -12,13 +13,19 @@ export class OrderController {
 
   async create({ body }: RequestContract<OrderModel>): Promise<HttpResponse> {
     const createdOrder = await this.clientUseCase.create(body)
+    
     if(createdOrder.isRight()) return successToCreate(createdOrder)
 
+    if(createdOrder.value instanceof OrderUseCaseClientNotFoundError) return badRequest('Client ID not found')
+
+    if(createdOrder.value instanceof OrderUseCaseProductNotFoundError) return badRequest('Product ID not found')
+
+    
     return internalError(createdOrder.value)
   }
 
   async load(): Promise<HttpResponse> {
     const loadOrder = await this.clientUseCase.load()
-    return success(loadOrder)
+    return success(loadOrder.value)
   }
 }
