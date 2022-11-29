4f2a05c94957926de864b185d74e6e860f93309d
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-29 01:08:11 -0300

    Add bind

--- src/controllers/order-controller.ts
+++ src/controllers/order-controller.ts
@@ -1,15 +1,20 @@
 import { RequestContract } from '@/main/contracts/http-contract'
 import { OrderUseCaseContract } from '@/use-case/contract'
 import { HttpResponse } from './contracts/http-responde'
-import { success, successToCreate } from './helpers/http-respnse'
+import { internalError, success, successToCreate } from './helpers/http-respnse'
 import { OrderModel } from './models'
 
 export class OrderController {
-  constructor(private clientUseCase: OrderUseCaseContract) {}
+  constructor(private clientUseCase: OrderUseCaseContract) {
+    this.create = this.create.bind(this)
+    this.load = this.load.bind(this)
+  }
 
   async create({ body }: RequestContract<OrderModel>): Promise<HttpResponse> {
     const createdOrder = await this.clientUseCase.create(body)
-    return successToCreate(createdOrder)
+    if(createdOrder.isRight()) return successToCreate(createdOrder)
+
+    return internalError(createdOrder.value)
   }
 
   async load(): Promise<HttpResponse> {
