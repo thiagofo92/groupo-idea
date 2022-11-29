78be1e5e499d916f2f967a382f9259851b0a57cc
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-29 00:36:37 -0300

    Add middleware to route client create

--- src/main/routers/routers.ts
+++ src/main/routers/routers.ts
@@ -1,13 +1,14 @@
 import { Router } from 'express'
 import { ExpressAdapter } from '../adapter/express-adapter'
 import { clientControllerFactory } from './factory'
+import { clientCreateMiddleware } from './middlewares'
 
 export class Routers {
   constructor(private readonly router: Router) {}
 
   private client(): void {
     const controller = clientControllerFactory()
-    this.router.post('/client/create', ExpressAdapter(controller.create))
+    this.router.post('/client/create', clientCreateMiddleware, ExpressAdapter(controller.create))
     this.router.get('/client/load', ExpressAdapter(controller.load))
   }
 
