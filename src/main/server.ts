b163311d269ff00764adf2d70b367f94f5bd40a0
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-29 11:38:51 -0300

    Start to set up swagger

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,15 +1,21 @@
 import Express from 'express'
-import { Routers } from './routers/routers'
+import { serve as SwaggerServer,  setup as SwaggerSetUp } from 'swagger-ui-express'
 
+import SwaggerJson from './routers/configs/swagger-config.json'
+import { Routers } from './routers/routers'
+import { configMiddleware } from './routers/middlewares/configs/config'
 const app = Express()
 
 const port = 4500
 const host = '0.0.0.0'
 
 app.use(Express.json())
+app.use(...configMiddleware)
 
 const routers = new Routers(Express.Router())
 app.use('/api', routers.factory())
+app.use('/api-docs', SwaggerServer, SwaggerSetUp(SwaggerJson))
+
 app.listen(port, host, () => {
   console.log(`Server is running ${host}:${port}`)
 })
