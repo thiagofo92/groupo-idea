735e5c9b8dda5a1fc00420d3dba9a19bcaad9eb1
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-28 23:45:25 -0300

    Add bind to functions

--- src/controllers/client-controller.ts
+++ src/controllers/client-controller.ts
@@ -1,19 +1,33 @@
 import { RequestContract } from '@/main/contracts/http-contract'
+import { ClientCreateServiceError } from '@/services/error'
 import { ClientUseCaseContract } from '@/use-case/contract'
+import { ClientCreateUseCaseError } from '@/use-case/error'
 import { HttpResponse } from './contracts/http-responde'
-import { success, successToCreate } from './helpers/http-respnse'
+import { badRequest, internalError, success, successToCreate } from './helpers/http-respnse'
 import { ClientModel } from './models'
 
 export class ClientController {
-  constructor(private clientUseCase: ClientUseCaseContract) {}
+  constructor(private clientUseCase: ClientUseCaseContract) {
+    this.create = this.create.bind(this)
+    this.load = this.load.bind(this)
+  }
 
   async create({ body }: RequestContract<ClientModel>): Promise<HttpResponse> {
     const createdClient = await this.clientUseCase.create(body)
-    return successToCreate(createdClient)
+    
+    if(createdClient.isRight()) return successToCreate(createdClient.value)
+
+    if(createdClient.value instanceof ClientCreateUseCaseError) return badRequest('Invalid age')
+
+    if(createdClient.value instanceof ClientCreateServiceError) return internalError(createdClient.value)
+
+    return internalError('Internal error')
   }
 
   async load(): Promise<HttpResponse> {
     const loadClient = await this.clientUseCase.load()
-    return success(loadClient)
+    if(loadClient.isRight()) return success(loadClient.value)
+
+    return internalError(loadClient.value)
   }
 }
