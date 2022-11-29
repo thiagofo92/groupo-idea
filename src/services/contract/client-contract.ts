9e962cdc5d762ef92ba41a907fbcdd80063e1fed
Thiago Ferreira <desenvolimento.oliveira@gmail.com>
2022-11-29 11:39:39 -0300

    Add load by id

--- src/services/contract/client-contract.ts
+++ src/services/contract/client-contract.ts
@@ -1,9 +1,10 @@
 import { ClientEntity } from '@/domain/entities'
 import { Either } from '@/shared/errors/Either'
-import { ClientCreateModel, ClientCreateResponseModel } from '@/use-case/model'
+import { ClientCreateResponseModel } from '@/use-case/model'
 import { ClientCreateServiceError, ClientLoadServiceError } from '../error'
 
 export interface ClientServiceContract {
   create: (data: ClientEntity) => Promise<Either<ClientCreateServiceError, ClientCreateResponseModel>> 
   load: () => Promise<Either<ClientLoadServiceError, ClientCreateResponseModel[]>>
+  loadById: (id: number) => Promise<Either<ClientLoadServiceError, ClientCreateResponseModel | null>>
 }
--- src/services/contract/product-contract.ts
+++ src/services/contract/product-contract.ts
@@ -6,4 +6,5 @@ import { ProductCreateServiceError, ProductLoadServiceError } from '../error'
 export interface ProductServiceContract {
   create: (data: ProductEntity) => Promise<Either<ProductCreateServiceError, ProducCreatetModel>>
   load: () => Promise<Either<ProductLoadServiceError, ProducCreatetModel[]>>
+  loadById: (id: number) => Promise<Either<ProductLoadServiceError, ProducCreatetModel | null>>
 }
